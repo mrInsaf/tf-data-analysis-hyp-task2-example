15410ec271498e42e342490ced63db8fa791ba01
mrInsaf <111053336+mrInsaf@users.noreply.github.com>
2023-04-12 15:28:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import wilcoxon
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 816831722  # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    stat, p = wilcoxon(hist.loc[a[0]], mod2.loc[a[1]])
+    if p <= 0.07:
+        return True
+    else:
+        return False
+    
