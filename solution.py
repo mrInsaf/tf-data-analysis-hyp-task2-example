7f2e156a283460e51f0935213827f4a650bf9bf5
mrInsaf <111053336+mrInsaf@users.noreply.github.com>
2023-04-13 12:19:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ from scipy.stats import wilcoxon
 chat_id = 816831722  # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    stat, p = wilcoxon(hist.loc[a[0]], mod2.loc[a[1]])
+    stat, p = wilcoxon(x, y)
     if p <= 0.07:
         return True
     else:
